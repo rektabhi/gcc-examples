cfd197ab17f266c085dd20037c2786bc304d62b7
Abhishek Bansal <abhishek.bansal@flipkart.com>
2023-01-28 11:30:56 +0530

    Added comments

--- vector_add/vector_add_2.c
+++ vector_add/vector_add_2.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 
+// Compile to assembly using:
+// gcc -O3 -S vector_add_2.c -o vector_add_2_with_optimisation.s
 int main() {
     int a[] = {1, 2, 3, 4, 5};
-    int b[] = {1, 2, 3, 4, 5};
+    int b[] = {2, 2, 3, 4, 5};
     int c[] = {1, 2, 3, 4, 5};
     int i = 0;
     for (i=0; i<5; i++) {
--- vector_add/vector_add_2_with_optimisation.s
+++ vector_add/vector_add_2_with_optimisation.s
@@ -5,24 +5,49 @@
 _main:                                  ; @main
 	.cfi_startproc
 ; %bb.0:
-	stp	x29, x30, [sp, #-16]!           ; 16-byte Folded Spill
-	mov	x29, sp
+	sub	sp, sp, #48
+	stp	x20, x19, [sp, #16]             ; 16-byte Folded Spill
+	stp	x29, x30, [sp, #32]             ; 16-byte Folded Spill
+	add	x29, sp, #32
 	.cfi_def_cfa w29, 16
 	.cfi_offset w30, -8
 	.cfi_offset w29, -16
+	.cfi_offset w19, -24
+	.cfi_offset w20, -32
+	mov	w8, #3
+	str	x8, [sp]
 Lloh0:
-	adrp	x0, l_.str@PAGE
+	adrp	x19, l_.str@PAGE
 Lloh1:
-	add	x0, x0, l_.str@PAGEOFF
+	add	x19, x19, l_.str@PAGEOFF
+	mov	x0, x19
+	bl	_printf
+	mov	w8, #4
+	str	x8, [sp]
+	mov	x0, x19
+	bl	_printf
+	mov	w8, #6
+	str	x8, [sp]
+	mov	x0, x19
+	bl	_printf
+	mov	w8, #8
+	str	x8, [sp]
+	mov	x0, x19
+	bl	_printf
+	mov	w8, #10
+	str	x8, [sp]
+	mov	x0, x19
 	bl	_printf
 	mov	w0, #0
-	ldp	x29, x30, [sp], #16             ; 16-byte Folded Reload
+	ldp	x29, x30, [sp, #32]             ; 16-byte Folded Reload
+	ldp	x20, x19, [sp, #16]             ; 16-byte Folded Reload
+	add	sp, sp, #48
 	ret
 	.loh AdrpAdd	Lloh0, Lloh1
 	.cfi_endproc
                                         ; -- End function
 	.section	__TEXT,__cstring,cstring_literals
 l_.str:                                 ; @.str
-	.asciz	"Hello World!"
+	.asciz	"%d\n"
 
 .subsections_via_symbols
